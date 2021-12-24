3b448c6e974f4b0e234512f853a0b00c07dce055
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-12-24 18:33:06 -0500

    add Frame

--- app/src/main/java/ca/josue/fishapp/MainActivity.java
+++ app/src/main/java/ca/josue/fishapp/MainActivity.java
@@ -1,8 +1,8 @@
 package ca.josue.fishapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
