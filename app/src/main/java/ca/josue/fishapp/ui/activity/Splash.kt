994e7be02875201be33d7a972a544f583fb80d79
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2022-01-01 11:57:45 -0500

    Update Splash.kt

--- app/src/main/java/ca/josue/fishapp/ui/activity/Splash.kt
+++ app/src/main/java/ca/josue/fishapp/ui/activity/Splash.kt
@@ -20,13 +20,10 @@ import java.lang.Thread.sleep
 import javax.inject.Inject
 
 @Suppress("DEPRECATION")
-@AndroidEntryPoint
 class Splash : AppCompatActivity() {
     private lateinit var myProgress: CircularProgressIndicator
     private lateinit var lottieWater : View
     private lateinit var lottieDoor : View
-    @Inject
-    lateinit var productResponseRepo : ProductResponseRepository
 
     companion object{
         val fishListProduct = arrayListOf<ProductResponse>()
@@ -40,57 +37,33 @@ class Splash : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         //myProgress = findViewById(R.id.myProgressBar)
         lottieWater = findViewById(R.id.animationViewWater)
-        runBlocking(Dispatchers.Default){
-            var result : List<Product>? = null
-            val task = listOf(
+        CoroutineScope(Dispatchers.Default).launch {
                 launch {
                     lottieWater.animate()
                         .translationY(lottieWater.height.toFloat()/3)
                         .alpha(1f)
                         .setListener(null)
-                    result = getAllProductsViaAPI()
-                    onResult(result)
-                },
-                launch{
-                    onResult(result)
-                    println("RESULT : ${result?.size}")
-
-                    val intent = Intent(this@Splash, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
+                    getAllProductsViaAPI()
                 }
-            )
-            task.joinAll()
-        }
-    }
-
-    private fun onResult(list : List<Product>?) {
-        list?.forEach { product ->
-            val prod = ProductResponse(
-                product.id,
-                product.name,
-                product.category.name,
-                product.price.toDouble(),
-                product.image,
-                product.description
-            )
-            productResponseRepo.insertProductResponse(prod)
+            delay(2500)
+            val intent = Intent(this@Splash, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 
     /**
      * Methode qui permet de récupèrer tous les produits
      * */
-    private suspend fun getAllProductsViaAPI() : MutableList<Product>? {
-        var list : MutableList<Product>? = null
+    private fun getAllProductsViaAPI() {
         val retrofitProduct = RetrofitClient.getApiService().getProducts()
         retrofitProduct.enqueue(object : Callback<List<Product>?> {
             override fun onResponse(call: Call<List<Product>?>, response: Response<List<Product>?>) {
                 if (!response.isSuccessful)
                     return
 
-                list = (response.body() as MutableList<Product>?)!!
-                list!!.forEach { product ->
+                val list = (response.body() as MutableList<Product>?)!!
+                list.forEach { product ->
                     val prod = ProductResponse(
                         product.id,
                         product.name,
@@ -107,8 +80,5 @@ class Splash : AppCompatActivity() {
                 println("Error retrofit ${t.message}")
             }
         })
-
-        onResult(list)
-        return list
     }
 }
